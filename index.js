7c6271f59a0be03e5495cb23f811085ae92ea116
yem1546 <87500366+yem1546@users.noreply.github.com>
2022-07-28 17:30:17 -0400

    MVP completed

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ function filterByWord(arr, word){
   let newArray = []
   for (let i = 0; i < arr.length; i++){
     if (arr[i].includes(word)){
-      newArray.unshift(arr[i])
+      newArray.push(arr[i])
     }
   }
   return newArray
